2f7b2e96a83aac676d28295f44d4a6abbff19c73
LillianHo5 <holillian5@gmail.com>
2025-06-20 16:18:44 -0700

    Update base.html and add stats

--- cookbook/recipes/urls.py
+++ cookbook/recipes/urls.py
@@ -5,8 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("recipe/<str:recipe_id>/", views.recipe_detail, name="recipe_detail"),
-    # path("search/", views.recipe_search, name="recipe_search"),
-    # path("list/", views.recipe_list, name="recipe_list"),
+    path("top/", views.top_recipes, name="top_recipes"),
     path("stats/", views.recipe_statistics, name="recipe_stats"),
     path(
         "ingredient-search/", views.ingredient_vector_search, name="ingredient_search"
